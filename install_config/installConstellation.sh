3fc2e70add94c088c6b23610895453e6396024f7
GabrielPora <gabriel.groener@gmail.com>
2017-01-25 12:52:35 +0200

    fixed script

--- install_config/installConstellation.sh
+++ install_config/installConstellation.sh
@@ -1,3 +1,3 @@
-wget https://github.com/jpmorganchase/constellation/releases/download/v0.0.1-alpha/ubuntu1604.zip
+wget https://github.com/jpmorganchase/constellation/releases/download/v0.0.1-alpha/ubuntu1604.zip;
 sudo apt install unzip
 sudo unzip ubuntu1604.zip -d /usr/local/bin
