a0f43a8a3e4ef9127f4c8ef7c6d7a53552360c22
GabrielPora <gabriel.groener@gmail.com>
2017-01-25 16:15:08 +0200

    better script

--- install_config/installQuorum.sh
+++ install_config/installQuorum.sh
@@ -1,20 +1,18 @@
+#!/bin/sh
 git clone https://github.com/jpmorganchase/quorum.git;
-read -n1 -r -p "Press space to continue once cloning is done..." key
-if [ "$key" = '' ]; then
-   cd quorum;
-fi
-sudo apt-get update
-sudo apt-get -y upgrade
-wget https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
-sudo tar -xvf go1.7.linux-amd64.tar.gz
-sudo mv go /usr/local
+cd ./quorum;
+sudo apt-get update;
+sudo apt-get upgrade -y;
+wget https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz;
+sudo tar -xvf go1.7.linux-amd64.tar.gz;
+sudo cp -r ./go/* /usr/local && sudo rm -Rf ./go/*;
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/projects/go # you might need to create this directory
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 # thes are for verification of go installation:
 # go version
 # go env
-cp /build/bin/* /usr/local/bin/ # try sudo cp -Rf build/* /usr/local/bin/ if it does not work
+cp -Rf build/* /usr/local/bin/
 make all
 # test:
 # make test
