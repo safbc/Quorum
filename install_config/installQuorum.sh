765b3d84233a88d32e148cf89282d69c436801cc
GabrielPora <gabriel.groener@gmail.com>
2017-01-25 12:45:16 +0200

    fixed minor issue of automated process when running script on linux

--- install_config/installQuorum.sh
+++ install_config/installQuorum.sh
@@ -1,5 +1,4 @@
-git clone https://github.com/jpmorganchase/quorum.git
-cd quorum
+git clone https://github.com/jpmorganchase/quorum.git;cd quorum
 sudo apt-get update
 sudo apt-get -y upgrade
 wget https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
@@ -11,7 +10,7 @@ export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 # thes are for verification of go installation:
 # go version
 # go env
-cp /build/bin/* /usr/local/bin/
+cp /build/bin/* /usr/local/bin/ # try sudo cp -Rf build/* /usr/local/bin/ if it does not work
 make all
 # test:
 # make test
